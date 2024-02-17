6e12a4dbc27558f6b359dab455e4fc9ecf7daea6
rvanasa <ryan.vandersmith@dfinity.org>
2024-02-17 15:48:19 -0700

    Misc

--- test/packages/ecdsa/rust/src/lib.rs
+++ test/packages/ecdsa/rust/src/lib.rs
@@ -1,22 +1,24 @@
-pub use ethers_core;
+use std::str::FromStr;
+
+use ethers_core::types::{Address, RecoveryMessage, Signature};
+
+pub fn ecdsa_verify(eth_address: String, message: String, signature: String) -> bool {
+    Signature::from_str(&signature)
+        .unwrap()
+        .verify(
+            RecoveryMessage::Data(message.into_bytes()),
+            Address::from_str(&eth_address).unwrap(),
+        )
+        .is_ok()
+}
 
 #[macro_export]
 macro_rules! canpack {
     () => {
-        use std::str::FromStr;
-
-        use $crate::ethers_core::types::{Address, RecoveryMessage, Signature};
-
         #[ic_cdk::query]
         #[candid::candid_method(query)]
         fn ecdsa_verify(eth_address: String, message: String, signature: String) -> bool {
-            Signature::from_str(&signature)
-                .unwrap()
-                .verify(
-                    RecoveryMessage::Data(message.into_bytes()),
-                    Address::from_str(&eth_address).unwrap(),
-                )
-                .is_ok()
+            $crate::ecdsa_verify(eth_address, message, signature)
         }
     };
 }
